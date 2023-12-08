d5379dc6074e712b0656f889d9f4e0754d60c97e
Nathan Joel <nathanjoel1@hotmail.co.uk>
2023-12-08 14:58:08 +0000

    Webdriver Manager

--- test_area.py
+++ test_area.py
@@ -3,13 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 from main import *
 
 @pytest.fixture
 def browser():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     yield driver
     # Teardown - close the browser window after the test
     driver.quit()
