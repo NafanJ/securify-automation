78a881705bfc57001faf5906359e02968db1f904
Nathan Joel <nathanjoel1@hotmail.co.uk>
2023-12-11 14:40:59 +0000

    Changed Driver Setup

--- test_area.py
+++ test_area.py
@@ -4,14 +4,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
 from main import *
 
 @pytest.fixture
 def browser():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver = webdriver.Chrome()
     yield driver
     # Teardown - close the browser window after the test
     driver.quit()
