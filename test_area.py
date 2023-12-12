6b0fbc10576f9360b9f6a5edb0feb52e652dc5e2
Nathan Joel <nathanjoel1@hotmail.co.uk>
2023-12-12 22:19:42 +0000

    Removed imports

--- test_area.py
+++ test_area.py
@@ -1,9 +1,5 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from selenium import webdriver
 
 from main import *
 
