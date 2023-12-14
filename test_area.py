ed82b93b943a260f4a19ef3617b8e313c0bd70f9
Nathan Joel <nathanjoel1@hotmail.co.uk>
2023-12-14 17:19:33 +0000

    Batch Test

--- test_area.py
+++ test_area.py
@@ -16,5 +16,11 @@ def test_form_submission(browser):
     test_value = form_submission(browser)
     if (test_value.text == "Thank's for signing up..!"):
         assert True
+
+def test_form_submisson_batch(browser):
+    for x in range(1, 10):
+        test_value = form_submission(browser)
+        if (test_value.text == "Thank's for signing up..!"):
+            assert True
         
 
