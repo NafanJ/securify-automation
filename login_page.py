b6fe75951075a7bc1e9555bf5285ab1bfa276469
Nathan Joel <nathanjoel1@hotmail.co.uk>
2023-12-19 19:27:10 +0000

    Submission Function now takes params
    Added Negative Testing
    
    Renamed main.py to login_page.py

--- login_page.py
+++ login_page.py
@@ -1,34 +1,37 @@
 from selenium.webdriver.common.by import By
 
-def form_submission(driver):
+def form_submission(driver, fname, lname, mail, phone, password):
     driver.get("https://nafanj.github.io/securify/")
     elem = driver.find_element(By.ID, "f-name")
-    elem.clear()
-    elem.send_keys("John")
+    elem.send_keys(fname)
 
     elem = driver.find_element(By.ID, "l-name")
-    elem.clear()
-    elem.send_keys("Doe")
+    elem.send_keys(lname)
 
     elem = driver.find_element(By.ID, "mail")
-    elem.clear()
-    elem.send_keys("test@gmail.com")
+    elem.send_keys(mail)
 
     elem = driver.find_element(By.ID, "phone")
-    elem.clear()
-    elem.send_keys("07519197543")
+    elem.send_keys(phone)
 
     elem = driver.find_element(By.ID, "user-password")
-    elem.clear()
-    elem.send_keys("Password123")
+    elem.send_keys(password)
 
     elem = driver.find_element(By.ID, "user-password-confirm")
-    elem.clear()
-    elem.send_keys("Password123")
+    elem.send_keys(password)
 
 
     elem = driver.find_element(By.XPATH, '//*[@id="btm"]/button')
     elem.click()
 
     elem = driver.find_element(By.XPATH, '/html/body/section/h1[2]')
+    return elem
+
+def form_fail(driver):
+    driver.get("https://nafanj.github.io/securify/")
+
+    elem = driver.find_element(By.XPATH, '//*[@id="btm"]/button')
+    elem.click()
+
+    elem = driver.find_element(By.XPATH,'/html/body/main/form/div/div[1]/div')
     return elem
--- test_area.py
+++ test_area.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
 
-from main import *
+from login_page import *
 
 @pytest.fixture
 def browser():
@@ -13,18 +13,23 @@ def browser():
 
 
 def test_form_submission(browser):
-    test_value = form_submission(browser)
+    test_value = form_submission(browser, 'John', 'Doe', 'johndoe@gmail.com', '+447519197543', 'Password123')
     if (test_value.text == "Thank's for signing up..!"):
         assert True
 
 def test_form_submisson_batch(browser):
     for x in range(1, 10):
-        test_value = form_submission(browser)
+        test_value = form_submission(browser, 'John', 'Doe', 'johndoe@gmail.com', '+447519197543', 'Password123')
         if (test_value.text == "Thank's for signing up..!"):
             assert True
         
 def test_form_submission_max(browser):
     for x in range(1, 100):
-        test_value = form_submission(browser)
+        test_value = form_submission(browser, 'John', 'Doe', 'johndoe@gmail.com', '+447519197543', 'Password123')
         if (test_value.text == "Thank's for signing up..!"):
             assert True  
+
+def test_fail(browser):
+    test_value = form_submission(browser, '', '', '', '', '')
+    if (test_value.text == "*This field is Required"):
+        assert True
